869abe6788c59430a05a69409153d8375c2f2119
channer72 <temioo@outlook.com>
2022-07-16 19:08:44 +1000

    changed timer logic, added countdown timer

--- playaudio.js
+++ playaudio.js
@@ -1,12 +1,20 @@
+const second = 1000;
+
 var ping = new Audio("sounds/chime1.mp3");
 var pingInterval;
 var timeUnit;
+var timerCount = 0;
 
-document.getElementById("start-button").addEventListener("click", timer)
+var secondsLeft = 0;
+var minutesLeft = 0;
+var hoursLeft = 0;
+
+document.getElementById("start-button").addEventListener("click", timer);
 
 function timer() {
+    console.log("start!");
     timerInit();
-    setInterval(playSound, pingInterval);
+    setInterval(timerIteration, second);
 
 }
 
@@ -25,8 +33,50 @@ function timerInit() {
     // console.log(pingInterval);
 }
 
+function timerIteration() {
+    timerCount++;
+    countdownToNext();
+    console.log(timerCount * 1000, pingInterval);
+    if (timerCount * 1000 - pingInterval === 0){
+        console.log("reset");
+        playSound();
+        timerCount = 0;
+    }
+}
+
+
 function playSound() {
     ping.load();
     ping.play();
 }
 
+function countdownToNext(){
+    formatCountdownTime();
+    
+    // adding a 0 to start of value if <10
+    const countdownTimer = `${hoursLeft}: ${minutesLeft}: ${secondsLeft}`
+    console.log(hoursLeft);
+    console.log(minutesLeft);
+    console.log(secondsLeft);
+
+    // current line to fix: figure out how to make element show timer
+    document.getElementById("iteration-countdown").innerHTML = countdownTimer;
+}
+
+/* separating countdown timer into hours, minutes and seconds */
+function formatCountdownTime(){
+    hoursLeft = Math.floor(timerCount / (60 * 24));
+    minutesLeft = Math.floor(timerCount / 60);
+    secondsLeft = timerCount % 60;
+
+    if (hoursLeft < 10){
+        hoursLeft = '0' + hoursLeft;
+    }
+    if (minutesLeft < 10){
+        minutesLeft = '0' + minutesLeft;
+    }
+    if (secondsLeft < 10){
+        secondsLeft = '0' + secondsLeft;
+    }
+
+}
